07da85ed048639bd26935896164a6f450069ec11
Riza <corodotc@gmail.com>
2025-04-09 15:11:08 +0700

    coment code warning

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,19 +1,19 @@
-import Layout from "@/components/Layout/page";
-import React from "react";
+// import Layout from "@/components/Layout/page";
+// import React from "react";
 
-interface UserDetailProps {
-  params: {
-    id: string;
-  };
-}
+// interface UserDetailProps {
+//   params: {
+//     id: string;
+//   };
+// }
 
-export default async function UserDetail({ params }: UserDetailProps) {
-  const { id } = await params;
-  return (
-    <Layout>
-      <div>
-        <p>User Detail Page {id}</p>
-      </div>
-    </Layout>
-  );
-}
+// export default async function UserDetail({ params }: UserDetailProps) {
+//   const { id } = await params;
+//   return (
+//     <Layout>
+//       <div>
+//         <p>User Detail Page {id}</p>
+//       </div>
+//     </Layout>
+//   );
+// }
